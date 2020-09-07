051f1f591e6139c023bbe90e765b41b2ee2687e2
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-09-07 11:43:38 +0100

    Get initial workshop code ready

---
+++ workshop/api.js
@@ -0,0 +1,7 @@
+export function login(email, password) {
+  // POST https://dogs-rest.herokuapp.com/v1/users/login/
+}
+
+export function getUser(token) {
+  // GET https://dogs-rest.herokuapp.com/v1/users/me/
+}
--- workshop/app.js
+++ workshop/app.js
@@ -1,24 +1,36 @@
 import h from "./create-element.js";
+import { login, getUser } from "./api.js";
 
-function Page() {
-  return h("div", { className: "page-layout" }, Header(), DogsList());
-}
+// El is short for  Element: i.e. these variables refer to DOM nodes
+const loginFormEl = LoginForm();
+const welcomeEl = h("main", {});
+const logoutEl = LogoutButton();
 
-function Header() {
-  return h("header", {}, h("span", { className: "logo" }, "🐶"), LoginForm());
-}
+// render the initial UI to the page
+const app = document.querySelector("#app");
+app.append(loginFormEl);
 
+// creates the form to log in:
+// <form><input type="email"><input type="password"><button>Log in</button>
 function LoginForm() {
   return h(
     "form",
-    {},
+    {
+      id: "loginForm",
+      onsubmit: (event) => {
+        event.preventDefault();
+      },
+    },
+    h("label", { for: "email" }, "Email"),
     h("input", {
+      id: "email",
       type: "email",
       name: "email",
       placeholder: "Email",
-      "aria-label": "Email",
     }),
+    h("label", { for: "password" }, "Password"),
     h("input", {
+      id: "password",
       type: "password",
       name: "password",
       placeholder: "Password",
@@ -28,15 +40,8 @@ function LoginForm() {
   );
 }
 
-function DogsList() {
-  return h("ul", {}, h("li", {}, "No dogs found"));
+// Creates the logout button
+// <button>Log out</button>
+function LogoutButton() {
+  return h("button", {}, "Log out");
 }
-
-const app = document.querySelector("#app");
-
-function render() {
-  app.innerHTML = "";
-  app.append(Page());
-}
-
-render();
